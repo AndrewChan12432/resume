168b1e24b5f48d32997b12105f45c5fb6e6076bb
Your Name <you@example.com>
2021-06-18 17:47:02 +0800

    delete useless staff

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className="">
+                <h2>AndrewChan</h2>
+            </div>
+        )
+    }
+}
